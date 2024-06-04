17c65bf8e61972154632d37a4106b8f0e1163b8d
0livrdavid <maxlex9854@gmail.com>
2024-06-04 02:26:29 -0300

    .

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    API_URL: process.env.API_URL,
+  },
+  reactStrictMode: true,
+};
 
 export default nextConfig;
